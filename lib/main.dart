fbbc5fe095d6afd8c5159454dacc0e17ed93d6dd
Hessian <cultivatordao@gmail.com>
2024-02-09 09:42:44 +0300

    V 0.0.1 Added dependecies:   firebase_core: ^2.25.4   cloud_firestore: ^4.15.4   firebase_core_platform_interface: ^5.0.0 Configured connection to Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,4 @@ import 'package:flutter/material.dart';
 import 'package:plantsility_app/main_app/main_app.dart';
 
 
-void main() {
-  runApp(const Plantsility());
-}
+void main() => runApp(const Plantsility());
